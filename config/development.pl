3a700fe12c5021f4b7793ac029f6a73ea0d39bac
Nishisi <e145735@gmail.com>
2016-10-10 18:01:40 +0900

    Auth情報の修正

--- config/development.pl
+++ config/development.pl
@@ -9,17 +9,17 @@
     
     Auth => +{
 	Twitter => +{
-	    consumer_key    => 'svqff6gCKMtDaLJXqzDHfKcCy',
-	    consumer_secret => 'aWV2YYdoHYYVHqDNVSPeSFlkQi9mKSXKX7AL0cNwlTr3fhPUjg',
+	    consumer_key    => '',
+	    consumer_secret => '',
 	    ssl => 1,
 	},
 	Github =>  +{
-	    client_id       => '41dc8be10a17353bf1d3',
-	    client_secret   => 'f6dfc229ed8b9215ee428367cd45b8f64e2dd84a',
+	    client_id       => '',
+	    client_secret   => '',
 	},
 	Line_Notify => +{
-	    client_id       => 'mijboN1vs24zxpRNndXrvr',
-	    client_secret   => 'x2e8NC7OM4ehvQeUUPfjKeqmg7OyDsxDp2JV3nEyrQS',
+	    client_id       => '',
+	    client_secret   => '',
 	}
     }
 };
