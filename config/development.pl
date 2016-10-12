9714ea707881d52a159a902aa9f9bfae24393228
K <x00.x7f@gmail.com>
2016-10-12 16:18:04 +0900

    修正

--- config/development.pl
+++ config/development.pl
@@ -1,15 +1,15 @@
 +{
     DBI => {
         connect_info => [
-        	"dbi:Pg:dbname=nishi_service;host=localhost;port=5432;",'', '', {
-        	}
+            "dbi:Pg:dbname=nishi_service;host=localhost;port=5432;",$ENV{PGUSER}, $ENV{PGPASS}, {
+            }
         ]
     },
     
     Auth => +{
-    	LINENotify => +{
-    	    client_id       => $ENV{LINE_CLIENT_ID},
-    	    client_secret   => $ENV{LINE_CLIENT_SECRET}
-    	}
+        LINENotify => +{
+            client_id       => $ENV{LINE_CLIENT_ID},
+            client_secret   => $ENV{LINE_CLIENT_SECRET}
+        }
     }
 };
